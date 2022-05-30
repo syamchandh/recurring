d030b5d416746fe16f94d369f111bb8733e55d9e
syamchandh <mrr6627@gmail.com>
2022-05-30 23:05:21 +0530

    commit 14

--- se_rec.py
+++ se_rec.py
@@ -15,6 +15,7 @@ import mysql.connector
 from tkinter import filedialog
 import json
 import shutil
+from math import *
 
 fbilldb = mysql.connector.connect(
     host="localhost", user="root", password="", database="fbillingsintgrtd", port="3306"
@@ -544,113 +545,12 @@ def search_invoice_recurring():
 
 #--------------------------calculator-recurring----------------------
 
-def calculator_recurring():
-  root = Tk()
-  root.geometry("312x324")
-  root.resizable(0, 0)  # prevent from resizing the window
-  root.title("GUI Calculator")
-
-  def btn_click(item):
-    global expression
-    expression = expression + str(item)
-    input_text.set(expression)
-
-  def bt_clear():
-    global expression
-    expression = ""
-    input_text.set("")
 
 
-  def bt_equal():
-    global expression
-    result = str(eval(expression))  # 'eval':This function is used to evaluates the string expression directly
-    input_text.set(result)
-    expression = ""
 
-  expression = ""
 
-  input_text = StringVar()
-
-  input_frame = Frame(root, width=312, height=50, bd=0, highlightbackground="#cce6ff", highlightcolor="black",
-                    highlightthickness=2)
-
-  input_frame.pack(side=TOP)
-
-# Let us create a input field inside the 'Frame'
-
-  input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#ffffff", bd=0,
-                    justify=RIGHT)
-
-  input_field.grid(row=0, column=0)
-
-  input_field.pack(ipady=10)  # 'ipady' internal padding
-
-# frame' for the button below the 'input_frame'
-
-  btns_frame = Frame(root, width=312, height=272.5, bg="grey")
-
-  btns_frame.pack()
-
-# first row
-
-  clear = Button(btns_frame, text="C", fg="black", width=32, height=3, bd=0, bg="#a3c2c2", cursor="hand2",
-               command=lambda: bt_clear()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
-
-  divide = Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-                command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
-
-# second row
-
-  seven = Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-               command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
-
-  eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-               command=lambda: btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
-
-  nine = Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-              command=lambda: btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
-
-  multiply = Button(btns_frame, text="*", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-                  command=lambda: btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
-
-# third row
-
-  four = Button(btns_frame, text="4", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-              command=lambda: btn_click(4)).grid(row=2, column=0, padx=1, pady=1)
-
-  five = Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-              command=lambda: btn_click(5)).grid(row=2, column=1, padx=1, pady=1)
-
-  six = Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-             command=lambda: btn_click(6)).grid(row=2, column=2, padx=1, pady=1)
-
-  minus = Button(btns_frame, text="-", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-               command=lambda: btn_click("-")).grid(row=2, column=3, padx=1, pady=1)
-
-# fourth row
-
-  one = Button(btns_frame, text="1", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-             command=lambda: btn_click(1)).grid(row=3, column=0, padx=1, pady=1)
-
-  two = Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-             command=lambda: btn_click(2)).grid(row=3, column=1, padx=1, pady=1)
-
-  three = Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2",
-               command=lambda: btn_click(3)).grid(row=3, column=2, padx=1, pady=1)
-
-  plus = Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-              command=lambda: btn_click("+")).grid(row=3, column=3, padx=1, pady=1)
-
-# fourth row
-
-  zero = Button(btns_frame, text="0", fg="black", width=21, height=3, bd=0, bg="#fff", cursor="hand2",
-              command=lambda: btn_click(0)).grid(row=4, column=0, columnspan=2, padx=1, pady=1)
-
-  point = Button(btns_frame, text=".", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-               command=lambda: btn_click(".")).grid(row=4, column=2, padx=1, pady=1)
-
-  equals = Button(btns_frame, text="=", fg="black", width=10, height=3, bd=0, bg="#ffa31a", cursor="hand2",
-                command=lambda: bt_equal()).grid(row=4, column=3, padx=1, pady=1)
+ 
+  
 
 
 #edit/view invoice in recurring 
@@ -1507,7 +1407,100 @@ def view_invoice_recurring():
       canbtn=Button(Mark_Invoice,compound = LEFT,image=cancel, text="Cancel", width=100).place(x=500, y=350)
     
    
-      
+#from tkinter import*
+
+# win=Tk()
+# win.title("Calculator")
+# win.geometry('315x510')
+# win.configure(background='grey')
+
+# def  btnclick(num):
+#     global operator
+#     operator=operator + str(num)
+#     _input.set(operator)
+
+# def clear():
+#     global operator
+#     operator=""
+#     _input.set("")
+
+# def answer():
+#     global operator
+#     ans=str(eval(operator))
+#     _input.set(ans)
+#     operator = ""
+
+# label=Label(win,font=('ariel' ,20,'bold'),text='Calculator',bg='grey',fg='black')
+# label.grid(columnspan=4)
+
+# _input=StringVar()
+# operator=""
+
+# display = Entry(win,font=('ariel' ,20,'bold'), textvariable=_input ,insertwidth=7 , bd=5 ,bg="white",justify='right')
+# display.grid(columnspan=4)
+
+# #------------------Row-1-------------------------------------------------------------------------------------------------------
+
+# b7=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="7",bg="grey", command=lambda: btnclick(7) )
+# b7.grid(row=2,column=0)
+
+# b8=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="8",bg="grey", command=lambda: btnclick(8) )
+# b8.grid(row=2,column=1)
+
+# b9=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="9",bg="grey", command=lambda: btnclick(9) )
+# b9.grid(row=2,column=2)
+
+# Add=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="+",bg="grey", command=lambda: btnclick("+") )
+# Add.grid(row=2,column=3)
+
+# #----------------Row-2---------------------------------------------------------------------------------------------------------------
+
+# b4=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="4",bg="grey", command=lambda: btnclick(4) )
+# b4.grid(row=3,column=0)
+
+# b5=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="5",bg="grey", command=lambda: btnclick(5) )
+# b5.grid(row=3,column=1)
+
+# b6=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="6",bg="grey", command=lambda: btnclick(6) )
+# b6.grid(row=3,column=2)
+
+# Sub=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="-",bg="grey", command=lambda: btnclick("-") )
+# Sub.grid(row=3,column=3)
+
+# #-------------Row-3------------------------------------------------------------------------------------------------------------------
+
+# b1=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="1",bg="grey", command=lambda: btnclick(1) )
+# b1.grid(row=4,column=0)
+
+# b2=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="2",bg="grey", command=lambda: btnclick(2) )
+# b2.grid(row=4,column=1)
+
+# b3=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="3",bg="grey", command=lambda: btnclick(3) )
+# b3.grid(row=4,column=2)
+
+# mul=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="*",bg="grey", command=lambda: btnclick("*") )
+# mul.grid(row=4,column=3)
+
+# #-----------------Row-4--------------------------------------------------------------------------------------------------------
+
+# b0=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="0",bg="grey", command=lambda: btnclick(0) )
+# b0.grid(row=5,column=0)
+
+# bc=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="c",bg="grey", command=clear)
+# bc.grid(row=5,column=1)
+
+# Decimal=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text=".",bg="grey", command=lambda: btnclick(".") )
+# Decimal.grid(row=5,column=2)
+
+# Div=Button(win,padx=16,pady=16,bd=4, fg="black", font=('ariel', 20 ,'bold'),text="/",bg="grey", command=lambda: btnclick("/") )
+# Div.grid(row=5,column=3)
+
+# #-----------------Row-6------------------------------------------------------------------------------------------------------------------
+
+# bequal=Button(win,padx=16,pady=16,bd=4,width = 16, fg="black", font=('ariel', 20 ,'bold'),text="=",bg="grey",command=answer)
+# bequal.grid(columnspan=4)
+
+# win.mainloop()
     #voidinvoice
     def voidinvoice_recurring():
       messagebox.askyesno("F-Billing Revolution","Are you sure to avoid this invoice?\nAll products will be placed back into stock and all payemnts will be voided.")
@@ -1614,6 +1607,65 @@ def view_invoice_recurring():
 
     w = Canvas(firFrame, width=1, height=65, bg="#b3b3b3", bd=0)
     w.pack(side="left", padx=5)
+    def calculator_recurring():
+        WIDTH = 267
+        HEIGHT = 403
+        DISPLAY_FONT = ("Arial", 30)
+        BUTTON_FONT = ("Arial", 25)
+        import tkinter as tk
+
+        window = tk.Tk()
+        window.title("Calculator")
+        window.geometry(f"{WIDTH}x{HEIGHT}")
+        window.resizable(0, 0)
+
+
+        def calculate():
+            try:
+                result = eval(display.get())
+                input_text.set(result)
+            except:
+                input_text.set("Wrong Input!")
+
+
+        # Display
+        input_text = tk.StringVar()
+        display = tk.Entry(window, font=DISPLAY_FONT, textvariable=input_text, bg="black", fg="white", width=12)
+        display.grid(row=0, columnspan=4, ipady=10)
+
+        # Buttons
+
+        # First row
+        tk.Button(window, text="(", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("("))).grid(row=1, column=0)
+        tk.Button(window, text=")", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(")"))).grid(row=1, column=1)
+        tk.Button(window, text="⌫", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.delete(len(display.get())-1)).grid(row=1, column=2)
+        tk.Button(window, text="=", font=BUTTON_FONT, fg="black", bg="light blue", width=3, height=1, command=calculate).grid(row=1, column=3)
+
+        # Second Row
+        tk.Button(window, text="1", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(1))).grid(row=2, column=0)
+        tk.Button(window, text="2", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(2))).grid(row=2, column=1)
+        tk.Button(window, text="3", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(3))).grid(row=2, column=2)
+        tk.Button(window, text="+", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("+"))).grid(row=2, column=3)
+
+        # Third row
+        tk.Button(window, text="4", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(4))).grid(row=3, column=0)
+        tk.Button(window, text="5", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(5))).grid(row=3, column=1)
+        tk.Button(window, text="6", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(6))).grid(row=3, column=2)
+        tk.Button(window, text="-", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("-"))).grid(row=3, column=3)
+
+        # Fourth Row
+        tk.Button(window, text="7", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(7))).grid(row=4, column=0)
+        tk.Button(window, text="8", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(8))).grid(row=4, column=1)
+        tk.Button(window, text="9", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(9))).grid(row=4, column=2)
+        tk.Button(window, text="x", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("*"))).grid(row=4, column=3)
+
+        # Fifth Row
+        tk.Button(window, text="C", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: input_text.set("")).grid(row=5, column=0)
+        tk.Button(window, text="0", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(0))).grid(row=5, column=1)
+        tk.Button(window, text=".", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("."))).grid(row=5, column=2)
+        tk.Button(window, text="/", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("/"))).grid(row=5, column=3)
+
+        window.mainloop()
 
     calc= Button(firFrame,compound="top", text="Open\nCalculator",relief=RAISED, image=photo9,bg="#f5f3f2", fg="black", height=55, bd=1, width=55,command=calculator_recurring)
     calc.pack(side="left", pady=3, ipadx=4)
