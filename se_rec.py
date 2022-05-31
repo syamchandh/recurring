1bb56c4b9f2f23afe4a5cd30438e3d68b9c2c0a3
syamchandh <mrr6627@gmail.com>
2022-05-31 22:29:03 +0530

    commit 15

--- se_rec.py
+++ se_rec.py
@@ -16,6 +16,9 @@ from tkinter import filedialog
 import json
 import shutil
 from math import *
+from datetime import timedelta
+from datetime import date
+import datetime
 
 fbilldb = mysql.connector.connect(
     host="localhost", user="root", password="", database="fbillingsintgrtd", port="3306"
@@ -202,7 +205,7 @@ def generate_recurring_invoice_success():
       main_rec_tree.insert(parent='', index='end', iid=i, text='', values=('',i[0], i[28], i[27], i[29], i[20], i[10]))
       j += 1
   #  if result=='ok':  
-  messagebox.showinfo("F-Billing Revolution 2022", "invoices successfully created.")
+  messagebox.showinfo("F-Billing Revolution 2022", "Recurring invoices successfully generated.")
  
 
 
@@ -1608,64 +1611,8 @@ def view_invoice_recurring():
     w = Canvas(firFrame, width=1, height=65, bg="#b3b3b3", bd=0)
     w.pack(side="left", padx=5)
     def calculator_recurring():
-        WIDTH = 267
-        HEIGHT = 403
-        DISPLAY_FONT = ("Arial", 30)
-        BUTTON_FONT = ("Arial", 25)
-        import tkinter as tk
+      print("hello")
 
-        window = tk.Tk()
-        window.title("Calculator")
-        window.geometry(f"{WIDTH}x{HEIGHT}")
-        window.resizable(0, 0)
-
-
-        def calculate():
-            try:
-                result = eval(display.get())
-                input_text.set(result)
-            except:
-                input_text.set("Wrong Input!")
-
-
-        # Display
-        input_text = tk.StringVar()
-        display = tk.Entry(window, font=DISPLAY_FONT, textvariable=input_text, bg="black", fg="white", width=12)
-        display.grid(row=0, columnspan=4, ipady=10)
-
-        # Buttons
-
-        # First row
-        tk.Button(window, text="(", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("("))).grid(row=1, column=0)
-        tk.Button(window, text=")", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(")"))).grid(row=1, column=1)
-        tk.Button(window, text="⌫", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.delete(len(display.get())-1)).grid(row=1, column=2)
-        tk.Button(window, text="=", font=BUTTON_FONT, fg="black", bg="light blue", width=3, height=1, command=calculate).grid(row=1, column=3)
-
-        # Second Row
-        tk.Button(window, text="1", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(1))).grid(row=2, column=0)
-        tk.Button(window, text="2", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(2))).grid(row=2, column=1)
-        tk.Button(window, text="3", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(3))).grid(row=2, column=2)
-        tk.Button(window, text="+", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("+"))).grid(row=2, column=3)
-
-        # Third row
-        tk.Button(window, text="4", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(4))).grid(row=3, column=0)
-        tk.Button(window, text="5", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(5))).grid(row=3, column=1)
-        tk.Button(window, text="6", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(6))).grid(row=3, column=2)
-        tk.Button(window, text="-", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("-"))).grid(row=3, column=3)
-
-        # Fourth Row
-        tk.Button(window, text="7", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(7))).grid(row=4, column=0)
-        tk.Button(window, text="8", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(8))).grid(row=4, column=1)
-        tk.Button(window, text="9", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(9))).grid(row=4, column=2)
-        tk.Button(window, text="x", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("*"))).grid(row=4, column=3)
-
-        # Fifth Row
-        tk.Button(window, text="C", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: input_text.set("")).grid(row=5, column=0)
-        tk.Button(window, text="0", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(0))).grid(row=5, column=1)
-        tk.Button(window, text=".", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("."))).grid(row=5, column=2)
-        tk.Button(window, text="/", font=BUTTON_FONT, fg="black", bg="white", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("/"))).grid(row=5, column=3)
-
-        window.mainloop()
 
     calc= Button(firFrame,compound="top", text="Open\nCalculator",relief=RAISED, image=photo9,bg="#f5f3f2", fg="black", height=55, bd=1, width=55,command=calculator_recurring)
     calc.pack(side="left", pady=3, ipadx=4)
@@ -1886,7 +1833,22 @@ def view_invoice_recurring():
     recurring_stop_check.place(x=225,y=95)
     recurring_stop_date = DateEntry(recurring_labelframe,width=20)
     recurring_stop_date.place(x=360,y=95)
-    recurring_recalc = Button(recurring_labelframe,compound=LEFT,image=recalc,text="Recalculate",width=80,height=12)
+    def recalculator():  
+      date_1 = invoice_date_entry.get()
+      print(date_1)
+          
+      if recurring_month_combo.get() == "Month(s)":
+        end_date = date_1 + datetime.timedelta(recurring_period_entry*365/12)
+
+      elif recurring_month_combo.get() == "Day(s)":
+        end_date = date_1 + str(datetime.timedelta(days=int(recurring_period_entry.get())))
+        
+          
+          
+      print(end_date)
+      recurring_next_invoice_date.delete(0,'end')
+      recurring_next_invoice_date.insert(0, end_date.strftime('%m/%d/%y'))
+    recurring_recalc = Button(recurring_labelframe,compound=LEFT,image=recalc,text="Recalculate",width=80,height=12,command=recalculator)
     recurring_recalc.place(x=540,y=70)
 
 
